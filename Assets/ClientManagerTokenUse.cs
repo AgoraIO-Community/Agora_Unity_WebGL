0a5ac3279922aad974be28d688e3d9c8b25ec9a5
Nathan Meier <groovyvideogames@gmail.com>
2022-08-04 10:29:39 -0700

    updates for client manager token use demo

---
+++ Assets/ClientManagerTokenUse.cs
@@ -0,0 +1,261 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using agora_gaming_rtc;
+using agora_utilities;
+
+public class ClientManagerTokenUse : MonoBehaviour
+{
+    [SerializeField] private string APP_ID = "YOUR_APPID";
+
+    [SerializeField] private string TOKEN_1 = "";
+
+    [SerializeField] private string CHANNEL_NAME_1 = "YOUR_CHANNEL_NAME_1";
+
+    public Text logText;
+    private Logger logger;
+    private IRtcEngine mRtcEngine = null;
+    private const float Offset = 100;
+
+    public Button joinButton, leaveButton;
+    public bool joinedChannel = false;
+    public bool useToken = false;
+    
+    private List<uint> remoteClientIDs;
+
+	public static ClientManagerTokenUse instance;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (!CheckAppId())
+        {
+            return;
+        }
+
+        InitEngine();
+        logger = new Logger(logText);
+        //channel setup.
+        remoteClientIDs = new List<uint>();
+		instance = this;
+    }
+
+    void Update()
+    {
+        PermissionHelper.RequestMicrophontPermission();
+        PermissionHelper.RequestCameraPermission();
+
+        if(joinedChannel){
+            joinButton.interactable = false;
+            leaveButton.interactable = true;
+        } else {
+            joinButton.interactable = true;
+            leaveButton.interactable = false;
+        }
+    }
+
+    bool CheckAppId()
+    {
+        logger = new Logger(logText);
+        logger.DebugAssert(APP_ID.Length > 10, "Please fill in your appId in VideoCanvas!!!!!");
+        return (APP_ID.Length > 10);
+    }
+
+    void InitEngine()
+    {
+        mRtcEngine = IRtcEngine.GetEngine(APP_ID);
+        mRtcEngine.SetChannelProfile(CHANNEL_PROFILE.CHANNEL_PROFILE_LIVE_BROADCASTING);
+
+        mRtcEngine.EnableAudio();
+        mRtcEngine.EnableVideo();
+        mRtcEngine.EnableVideoObserver();
+        mRtcEngine.SetClientRole(CLIENT_ROLE_TYPE.CLIENT_ROLE_BROADCASTER);
+
+        mRtcEngine.OnJoinChannelSuccess = EngineOnJoinChannelSuccessHandler;
+        mRtcEngine.OnLeaveChannel = EngineOnLeaveChannelHandler;
+
+        mRtcEngine.OnUserJoined += EngineOnUserJoinedHandler;
+        mRtcEngine.OnUserOffline += EngineOnUserOfflineHandler;
+
+        mRtcEngine.OnError += EngineOnErrorHandler;
+
+    }
+
+    public void JoinChannel()
+    {
+        if(!useToken){
+            mRtcEngine.JoinChannel(TOKEN_1, CHANNEL_NAME_1, "", 0, new ChannelMediaOptions(true, true, true, true));
+        } else {
+            TokenClientNew.Instance.SetRtcEngineInstance(mRtcEngine);
+            TokenClientNew.Instance.GetTokens(CHANNEL_NAME_1, 0, (token, rtm) =>
+            {
+                TOKEN_1 = token;
+                Debug.Log(gameObject.name + " Got rtc token:" + TOKEN_1);
+                mRtcEngine.JoinChannelByKey(TOKEN_1, CHANNEL_NAME_1);
+            });
+        }
+        joinedChannel = true;
+    }
+
+    public void LeaveChannel()
+    {
+        mRtcEngine.LeaveChannel();
+        joinedChannel = false;
+    }
+
+    void OnApplicationQuit()
+    {
+        Debug.Log("OnApplicationQuit");
+        if (mRtcEngine != null)
+        {
+
+            mRtcEngine.DisableVideoObserver();
+            IRtcEngine.Destroy();
+        }
+    }
+
+    void EngineOnJoinChannelSuccessHandler(string channelId, uint uid, int elapsed)
+    {
+        logger.UpdateLog(string.Format("sdk version: ${0}", IRtcEngine.GetSdkVersion()));
+        logger.UpdateLog(string.Format("EngineOnJoinChannelSuccess channelId: {0}, uid: {1}, elapsed: {2}", CHANNEL_NAME_1, uid,
+            elapsed));
+        RespawnLocal(CHANNEL_NAME_1);
+    }
+
+    void EngineOnLeaveChannelHandler(RtcStats rtcStats)
+    {
+        logger.UpdateLog(string.Format("OnLeaveChannelHandler channelId: {0}", CHANNEL_NAME_1));
+		DestroyVideoView(CHANNEL_NAME_1, 0);
+    }
+
+    void EngineOnErrorHandler(int err, string message)
+    {
+        logger.UpdateLog(string.Format("UserErrorHandler err: {0}, message: {1}", err,
+            message));
+    }
+
+    void EngineOnUserJoinedHandler(uint uid, int elapsed)
+    {
+        logger.UpdateLog(string.Format("OnUserJoinedHandler channelId: {0} uid: ${1} elapsed: ${2}", CHANNEL_NAME_1,
+            uid, elapsed));
+        makeVideoView(CHANNEL_NAME_1, uid);
+        remoteClientIDs.Add(uid);
+    }
+
+    void EngineOnUserOfflineHandler(uint uid, USER_OFFLINE_REASON reason)
+    {
+        logger.UpdateLog(string.Format("OnUserOffLine uid: ${0}, reason: ${1}", uid, (int)reason));
+        DestroyVideoView(CHANNEL_NAME_1, uid);
+        remoteClientIDs.Remove(uid);
+    }
+
+    public void RespawnLocal(string channelName)
+    {
+        GameObject go = GameObject.Find(channelName + "_0");
+        if (go != null)
+        {
+            go.name = "Destroying";
+            Destroy(go);
+            makeVideoView(channelName, 0);
+        }  else {
+			makeVideoView(channelName, 0);
+		}
+    }
+
+    public void RespawnRemote()
+    {
+        if (LastRemote != null)
+        {
+            string[] strs = LastRemote.name.Split('_');
+            string channel = strs[0];
+            uint uid = uint.Parse(strs[1]);
+            LastRemote.name = "_Destroyer";
+            Destroy(LastRemote);
+            Debug.LogWarningFormat("Remaking video surface for  uid:{0} channel:{1}", uid, channel);
+            makeVideoView(channel, uid);
+        }
+    }
+
+    GameObject LastRemote = null;
+
+    private void makeVideoView(string channelId, uint uid)
+    {
+        string objName = channelId + "_" + uid.ToString();
+        GameObject go = GameObject.Find(objName);
+        if (!ReferenceEquals(go, null))
+        {
+            return; // reuse
+        }
+
+
+        // create a GameObject and assign to this new user
+        VideoSurface videoSurface = makeImageSurface(objName);
+        if (!ReferenceEquals(videoSurface, null))
+        {
+			
+		Debug.Log("surface is..." + videoSurface);
+            // configure videoSurface
+            videoSurface.SetForUser(uid);
+            videoSurface.SetEnable(true);
+            videoSurface.SetVideoSurfaceType(AgoraVideoSurfaceType.RawImage);
+            // make the object draggable
+            videoSurface.gameObject.AddComponent<UIElementDragger>();
+
+            if (uid != 0)
+            {
+                LastRemote = videoSurface.gameObject;
+            }
+        }
+    }
+
+    // Video TYPE 2: RawImage
+    public VideoSurface makeImageSurface(string goName)
+    {
+        GameObject go = new GameObject();
+
+        if (go == null)
+        {
+            return null;
+        }
+
+        go.name = goName;
+        // make the object draggable
+        go.AddComponent<UIElementDrag>();
+        // to be renderered onto
+        go.AddComponent<RawImage>();
+
+        GameObject canvas = GameObject.Find("VideoCanvas");
+        if (canvas != null)
+        {
+            go.transform.SetParent(canvas.transform);
+            Debug.Log("add video view");
+        }
+        else
+        {
+            Debug.Log("Canvas is null video view");
+        }
+
+        // set up transform
+        go.transform.Rotate(0f, 0.0f, 180.0f);
+        float xPos = Random.Range(Offset - Screen.width / 2f, Screen.width / 2f - Offset);
+        float yPos = Random.Range(Offset, Screen.height / 2f - Offset);
+        Debug.Log("position x " + xPos + " y: " + yPos);
+        go.transform.localPosition = new Vector3(xPos, yPos, 0f);
+        go.transform.localScale = new Vector3(1.5f, 1f, 1f);
+
+        // configure videoSurface
+        VideoSurface videoSurface = go.AddComponent<VideoSurface>();
+        return videoSurface;
+    }
+
+    private void DestroyVideoView(string channelId, uint uid)
+    {
+        string objName = channelId + "_" + uid.ToString();
+        GameObject go = GameObject.Find(objName);
+        if (!ReferenceEquals(go, null))
+        {
+            Object.Destroy(go);
+        }
+    }
+}
