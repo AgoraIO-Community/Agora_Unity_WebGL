9ce43986be342d393a05d716d34c9b60cb6f3e13
Nathan Meier <groovyvideogames@gmail.com>
2022-09-29 13:03:20 -0700

    new spatial audio demo scene.

---
+++ Assets/SpriteLayerHandler.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLayerHandler : MonoBehaviour {
+
+	SpriteRenderer sprite;
+
+	// Use this for initialization
+	void Start () {
+		sprite = GetComponent<SpriteRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		sprite.sortingOrder = -(int)Mathf.Round(transform.position.y);
+	}
+}
