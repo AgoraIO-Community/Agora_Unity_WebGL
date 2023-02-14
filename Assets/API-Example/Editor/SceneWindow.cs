d05d3e954dd1b08554130e22723d40758ddd6bf0
rickagora <rick@agora.io>
2023-02-13 18:31:26 -0800

    Update HomeMemu UIs and file locations

---
+++ Assets/API-Example/Editor/SceneWindow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+namespace Agora_RTC_Plugin.API_Example.Editor
+{
+    public class SceneEditorWindow : EditorWindow
+    {
+        List<SceneAsset> m_SceneAssets = new List<SceneAsset>();
+
+        // Add menu item named "Example Window" to the Window menu
+        [MenuItem("Build/List Scenes", false, 1)]
+        public static void ShowWindow()
+        {
+            //Show existing window instance. If one doesn't exist, make one.
+            EditorWindow.GetWindow(typeof(SceneEditorWindow));
+        }
+
+        void OnGUI()
+        {
+            GUILayout.Space(8);
+            if (GUILayout.Button("Search and Add Scene files"))
+            {
+                List<EditorBuildSettingsScene> editorBuildSettingsScenes = new List<EditorBuildSettingsScene>();
+                foreach (var file in System.IO.Directory.EnumerateFiles(".", "*.unity", System.IO.SearchOption.AllDirectories))
+                {
+                    string scenePath = file.Remove(0, 2);
+                    UnityEngine.Debug.Log(scenePath);
+                    editorBuildSettingsScenes.Add(new EditorBuildSettingsScene(scenePath, true));
+                }
+                EditorBuildSettings.scenes = editorBuildSettingsScenes.ToArray();
+            }
+
+            GUILayout.Space(8);
+            if (GUILayout.Button("Clear Scene files"))
+            {
+                EditorBuildSettings.scenes = null;
+            }
+        }
+
+
+    }
+}
